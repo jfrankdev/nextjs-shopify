27c4f22f060b32b1b7b007c352b969040f6db63a
James Frank <jfrankdev90@gmail.com>
2023-01-16 11:37:22 -0600

    remove anti flicker css from dangerouslysethtml

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -56,7 +56,6 @@ class MyDocument extends Document {
               __html: `
               if(!(window.location.href.includes("nogtm"))){
                 <!-- anti-flicker snippet (recommended)  -->
-                <style>.async-hide { opacity: 0 !important} </style>
                 (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
                 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
                 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
